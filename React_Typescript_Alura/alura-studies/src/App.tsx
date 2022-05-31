00412329765ea1bf4565dbe08d7e5af883c2aedf
Nathan-Rgs <89109761+Nathan-Rgs@users.noreply.github.com>
2022-05-31 17:05:27 -0300

    New Project

---
+++ React_Typescript_Alura/alura-studies/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Botao from "./components/Buttons/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Botao></Botao>
+    </div>
+  );
+}
+
+export default App;
