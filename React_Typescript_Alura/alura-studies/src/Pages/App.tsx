10cdbcadb0477e43bb5940d1ebe0bbe4be29cd50
Nathan-Rgs <89109761+Nathan-Rgs@users.noreply.github.com>
2022-08-16 19:55:18 -0300

    Studies

--- React_Typescript_Alura/alura-studies/src/Pages/App.tsx
+++ React_Typescript_Alura/alura-studies/src/Pages/App.tsx
@@ -7,10 +7,21 @@ import style from "./App.module.scss";
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas((tarefasAnteriores) =>
+      tarefasAnteriores.map((tarefa) => ({
+        ...tarefa,
+        selecionado: tarefa.id === tarefaSelecionada.id ? true : false,
+      }))
+    );
+  }
   return (
     <div className={style.AppStyle}>
       <Form setTarefas={setTarefas}></Form>
-      <List tarefas={tarefas}></List>
+      <List tarefas={tarefas} selecionaTarefa={selecionaTarefa}></List>
       <Cronometer></Cronometer>
     </div>
   );
--- React_Typescript_Alura/alura-studies/src/components/List/index.tsx
+++ React_Typescript_Alura/alura-studies/src/components/List/index.tsx
@@ -2,13 +2,18 @@ import { ITarefa } from "../../types/tarefas";
 import Item from "./Item";
 import style from "./List.module.scss";
 
-function Lista({ tarefas }: { tarefas: ITarefa[] }) {
+interface Props {
+  tarefas: ITarefa[];
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
   return (
     <aside className={style.listaTarefas}>
       <h2>Estudos do Dia:</h2>
       <ul>
-        {tarefas.map((item, index) => (
-          <Item key={index} {...item} />
+        {tarefas.map((item) => (
+          <Item selecionaTarefa={selecionaTarefa} key={item.id} {...item} />
         ))}
       </ul>
     </aside>
